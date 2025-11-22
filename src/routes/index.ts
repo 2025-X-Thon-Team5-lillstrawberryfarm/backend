938ae695506d42bb6bce015f2b0b0db13abd3134
hilyhily334 <hilyhily334@gmail.com>
2025-11-22 17:40:04 +0900

    ci: ci/cd 수정

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from 'express';
 const mainRouter = Router();
 
 mainRouter.get('/', (_req: Request, res: Response) => {
-  res.json({ message: 'Hello from the API root!' });
+  res.json({ message: 'Hello from the API root' });
 });
 
 export default mainRouter;
